7d5143922e47ce88d8fa6666278c5c9213c65fb6
solomonrazak <solomonrazak99@gmail.com>
2024-07-12 09:45:00 -0700

    api server

---
+++ api/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+app.use("api/test", (req,res) => {
+    res.send("it works");
+})
+app.listen(8800, () => {
+    console.log("server is running!");
+})
