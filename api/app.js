c1e044c0084f0db82efee086d5112c90a95403c1
solomonrazak <solomonrazak99@gmail.com>
2024-07-18 09:31:51 -0700

    Remove nested Git repository from client directory and add client to main repository

--- api/app.js
+++ api/app.js
@@ -2,9 +2,11 @@ import express from 'express';
 import postRoute from './routes/post.route.js';
 import authRoute from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors({origin: process.env.CLIENT_URL, credentials: true}))
 app.use(express.json()) // to be able use or pass json objects
 app.use(cookieParser());
 
